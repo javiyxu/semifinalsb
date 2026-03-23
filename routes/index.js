4c6d6cd4a1c6abda2baadb35adf10f8a61b79aac
alas <janreytanjutco15@gmail.com>
2026-03-23 11:21:07 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,12 @@
-// Meetings
-router.get('/meetings', meetingController.getAllMeetings);
-router.get('/meetings/:id', meetingController.getMeetingById);
-router.post('/meetings', meetingController.createMeeting);
-router.put('/meetings/:id', meetingController.updateMeeting);      // <- added :id
-router.delete('/meetings/:id', meetingController.deleteMeeting);   // <- added :id
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/meetingController');
 
-// Zoom Sessions
-router.get('/zoom-sessions', meetingController.getAllZoomSessions);
-router.get('/zoom-sessions/:id', meetingController.getZoomSessionById);
-router.post('/zoom-sessions', meetingController.createZoomSession);
-router.put('/zoom-sessions/:id', meetingController.updateZoomSession);
-router.delete('/zoom-sessions/:id', meetingController.deleteZoomSession);
+// Meetings CRUD
+router.get('/meetings', controller.getAllMeetings);
+router.get('/meetings/:id', controller.getMeetingById);
+router.post('/meetings', controller.createMeeting);
+router.put('/meetings/:id', controller.updateMeeting);
+router.delete('/meetings/:id', controller.deleteMeeting);
 
-// Study Sessions
-router.get('/study-sessions', meetingController.getAllStudySessions);
-router.get('/study-sessions/:id', meetingController.getStudySessionById);
-router.post('/study-sessions', meetingController.createStudySession);
-router.put('/study-sessions/:id', meetingController.updateStudySession);
-router.delete('/study-sessions/:id', meetingController.deleteStudySession);
-
-// Participants
-router.get('/participants', meetingController.getAllParticipants);
-router.get('/participants/:id', meetingController.getParticipantById);
-router.post('/participants', meetingController.createParticipant);
-router.put('/participants/:id', meetingController.updateParticipant);
-router.delete('/participants/:id', meetingController.deleteParticipant);
-
-// Agendas
-router.get('/agendas', meetingController.getAllAgendas);
-router.get('/agendas/:id', meetingController.getAgendaById);
-router.post('/agendas', meetingController.createAgenda);
-router.put('/agendas/:id', meetingController.updateAgenda);
-router.delete('/agendas/:id', meetingController.deleteAgenda);
+module.exports = router;
