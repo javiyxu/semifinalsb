f9fd3848d320ae0abcc61148a91072dfd08670b2
alas <janreytanjutco15@gmail.com>
2026-03-23 11:24:07 +0800

    Remove unnecessary line breaks in index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/agendaController');
 
-// Agendas CRUD
 router.get('/agendas', controller.getAllAgendas);
 router.get('/agendas/:id', controller.getAgendaById);
 router.post('/agendas', controller.createAgenda);
